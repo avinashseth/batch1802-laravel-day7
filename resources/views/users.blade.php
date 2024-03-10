cd1ea4c2e59b37acbbdafc0b0639f2a478bd0457
Avinash Seth <bloggerbeg@gmail.com>
2024-03-10 22:30:14 +0530

    loops

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,3 +1,18 @@
 @foreach($users as $user)
-    <p>{{ $user->name }}</p>
+
+    @if($loop->first)
+        <p>This is the first iteration</p>
+    @endif
+
+    @if($loop->last)
+        <p>This is the last iteration</p>
+    @endif
+
+    <p>{{ $user->name . ' Index: ' . $loop->index }}</p>
 @endforeach
+
+@forelse($users as $user)
+    <p>{{ $user->name }}</p>
+@empty
+    <p>List of users are empty</p>
+@endforelse
